263dcf9cd536cd90669ddd05e84ed2f9b6c88f4b
vikas0693 <vikas06sharma93@gmail.com>
2020-09-16 02:15:55 +0530

    Adding basic authentication to requests. till video 78

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HelloWorldBean } from 'src/app/welcome/welcome.component';
 
 @Injectable({
@@ -11,6 +11,19 @@ export class WelcomeDataService {
   constructor(private route: ActivatedRoute, private http:HttpClient) { }
 
   executeHelloWorldBeanService(){
-    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world/path-variable/vikas');
+    let authString = this.createBasicAuthenticationHttpHeader();
+    let header = new HttpHeaders({
+      Authorization: authString
+    });
+    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world/path-variable/vikas',{
+      headers: header
+    });
+  }
+
+  createBasicAuthenticationHttpHeader(){
+    const username="in28minutes";
+    const password="dummy";
+    let basicAuthHeaderString = 'Basic '+ window.btoa(username + ':' + password)
+    return basicAuthHeaderString;
   }
 }
